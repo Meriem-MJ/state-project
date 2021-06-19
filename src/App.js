de000d7dd7bc56d6327a099e058cf9c4ff4eb67f
Meriem Jebara <jebara.meriem133@gmail.com>
2021-06-19 08:43:57 +0200

    counter

--- src/App.js
+++ src/App.js
@@ -7,16 +7,34 @@ export class App extends Component {
   state = {
     visibility: false
   };
+constructor(props){
+  super(props);
+  this.state = {
+    digit: 0,
+  };
+}
   handleToggleVisibility = () => {
     this.setState(prevState => ({visibility: !prevState.visibility}));
   }
+
+componentDidMount = () => {
+setInterval(() => {
+this.setState(prevState => ({
+  digit: prevState.digit + 1
+}))
+}, 1000)
+}
   render() {
+    
     return (
-      <div>
-        <Profile fullName ="Meriem Jebara" bio ="Student in GoMyCode" profession ="Profession: Logistic Assistant" visibility={this.state.visibility} handleToggleVisibility={this.handleToggleVisibility}/>
-      </div>
+
+        <div>
+          <Profile fullName ="Meriem Jebara" bio ="Student in GoMyCode" profession ="Profession: Logistic Assistant" visibility={this.state.visibility} handleToggleVisibility={this.handleToggleVisibility}/>
+          <div className="counter">Counter: {this.state.digit}</div>
+        </div>
     )
-  }
+ 
+}
 }
 
 export default App
